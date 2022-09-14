94155cc11e576c7361fa2b16763122df6273e26c
almat <almatorta00@freemail.hu>
2022-09-14 16:57:04 +0200

    Version 0.123

--- app/src/main/java/com/example/firebaselogin/activities/MainActivity.java
+++ app/src/main/java/com/example/firebaselogin/activities/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         initNetworkStateListener();
         initGui();
 
-
         etBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
